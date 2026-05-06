03fe8d02cb536a4f0ba5a338f9863c3789e072dd
stanislav <stanislav.glumov.2006@mail.ru>
2026-05-06 16:48:49 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Directionality(
+      textDirection: TextDirection.ltr,
+      child: Container(
+        color: Color(0xFFFFFFFF),
+        child: App(),
+      ),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo list',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      // home: const HomeScreen(),    
+    );
+  }
+}
